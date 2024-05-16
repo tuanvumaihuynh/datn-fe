91bf42369cfce5b2eda06aa78e4f183f32836434
tuanvumaihuynh <tuanvux01@gmail.com>
2024-05-16 10:58:38 +0700

    update: toast when copy

--- src/hooks/useClipboard.ts
+++ src/hooks/useClipboard.ts
@@ -1,8 +1,14 @@
+import { useToast } from "@/components/ui/toast/use-toast";
+
 export default (): {
   copyToClipboard: (text: string) => void;
 } => {
+  const { toast } = useToast();
   const copyToClipboard = (text: string) => {
     navigator.clipboard.writeText(text);
+    toast({
+      title: "Copied to clipboard",
+    });
   };
   return {
     copyToClipboard,
