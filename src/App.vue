f7fabce5026d9ccc138f0540850f9f11bcc1b37f
tuanvumaihuynh <tuanvux01@gmail.com>
2024-05-17 00:45:42 +0700

    add favicon, title

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,15 @@
 </template>
 
 <script setup lang="ts">
-import { useColorMode } from "@vueuse/core";
+import { useColorMode, useFavicon, useTitle } from "@vueuse/core";
 import Toaster from "@/components/ui/toast/Toaster.vue";
+
+import Logo from "@/assets/logo.png";
+
 useColorMode();
+const icon = useFavicon();
+const title = useTitle();
+
+icon.value = Logo;
+title.value = "ProjectEnergyAdmin";
 </script>
