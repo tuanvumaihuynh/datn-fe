606e96b95d15debffbc753b0fbddb8fcfd376454
tuanvumaihuynh <tuanvux01@gmail.com>
2024-05-17 22:33:41 +0700

    Update login page

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,66 +1,116 @@
 <template>
-  <div
-    class="flex flex-col justify-center items-center h-screen bg-background"
-    :style="{
-      'background-image': ' url(' + LooperPatern + ')',
-      'background-size': 'cover',
-      'background-position': 'center',
-      'background-repeat': 'no-repeat',
-    }"
-  >
-    <Card class="w-full max-w-sm">
-      <form @submit="onSubmit">
-        <CardHeader>
-          <CardTitle class="text-2xl">Sign in</CardTitle>
-          <CardDescription> to continue to ProjectEnergyAdmin </CardDescription>
-        </CardHeader>
-        <CardContent class="grid gap-4">
-          <div class="grid gap-2">
-            <Label for="username">Username</Label>
-            <Input
-              id="username"
-              type="username"
-              placeholder="username"
-              required
-              v-model="username"
-            />
-            <span class="text-red-500">{{ errors.username }}</span>
+  <section class="bg-white">
+    <div class="lg:grid lg:min-h-screen lg:grid-cols-12">
+      <section
+        class="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6"
+      >
+        <img
+          alt=""
+          src="https://images.unsplash.com/photo-1481127303226-3f47f8af862d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          class="absolute inset-0 h-full w-full object-cover opacity-80"
+        />
+
+        <div class="hidden lg:relative lg:block lg:p-12">
+          <a class="block text-white" href="#">
+            <span class="sr-only">Home</span>
+            <Logo class="h-12 w-12 object-contain" />
+          </a>
+
+          <h2
+            class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 text-transparent py-1 bg-clip-text mt-6 text-2xl font-bold sm:text-3xl md:text-4xl"
+          >
+            Welcome to ProjectEnergyAdmin
+          </h2>
+
+          <p class="mt-4 leading-relaxed text-white/90">
+            Monitoring and collecting energy data from devices.
+          </p>
+        </div>
+      </section>
+
+      <main
+        class="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
+      >
+        <div class="max-w-xl lg:max-w-3xl">
+          <div class="relative -mt-16 block lg:hidden">
+            <a
+              class="inline-flex size-16 items-center justify-center rounded-full bg-white text-blue-600 sm:size-20"
+              href="#"
+            >
+              <span class="sr-only">Home</span>
+              <Logo class="h-12 w-12 object-contain" />
+            </a>
+
+            <h1
+              class="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl"
+            >
+              Welcome to ProjectEnergyAdmin
+            </h1>
+
+            <p class="mt-4 leading-relaxed text-gray-500">
+              Monitoring and collecting energy data from devices.
+            </p>
           </div>
-          <div class="grid gap-2">
-            <Label for="password">Password</Label>
-            <Input
-              id="password"
-              type="password"
-              placeholder="********"
-              required
-              v-model="password"
-            />
-            <span class="text-red-500">{{ errors.password }}</span>
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button class="w-full" type="submit" :disabled="isLoading">
-            <LoaderCircle v-if="isLoading" class="w-4 h-4 mr-2 animate-spin" />
-            Sign in
-          </Button>
-        </CardFooter>
-      </form>
-    </Card>
-  </div>
+
+          <form
+            @submit="onSubmit"
+            class="mt-8 flex flex-col min-w-[25vw] gap-6"
+          >
+            <h1 class="text-2xl">Sign in</h1>
+            <div class="grid gap-2">
+              <Label for="username">Username</Label>
+              <Input
+                id="username"
+                type="username"
+                placeholder="username"
+                required
+                v-model="username"
+              />
+              <span class="text-red-500">{{ errors.username }}</span>
+            </div>
+
+            <div class="grid gap-2">
+              <Label for="password">Password</Label>
+              <div class="relative items-center">
+                <Input
+                  id="password"
+                  :type="showPassword ? 'text' : 'password'"
+                  placeholder="********"
+                  required
+                  v-model="password"
+                />
+                <span
+                  class="absolute end-0 inset-y-0 rounded-lg flex items-center justify-center p-2 hover:bg-gray-100 cursor-pointer"
+                  @click="showPassword = !showPassword"
+                >
+                  <component
+                    :is="showPassword ? EyeOff : Eye"
+                    class="size-4 text-muted-foreground"
+                  />
+                </span>
+              </div>
+              <span class="text-red-500">{{ errors.password }}</span>
+            </div>
+
+            <div class="col-span-6 sm:flex sm:items-center sm:gap-4">
+              <Button class="w-full" type="submit" :disabled="isLoading">
+                <LoaderCircle
+                  v-if="isLoading"
+                  class="w-4 h-4 mr-2 animate-spin"
+                />
+                Sign in
+              </Button>
+            </div>
+          </form>
+        </div>
+      </main>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
-import LooperPatern from "@/assets/looper-pattern.svg";
-import { LoaderCircle } from "lucide-vue-next";
+import { Eye, EyeOff, LoaderCircle } from "lucide-vue-next";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
@@ -69,13 +119,19 @@ import { useField, useForm } from "vee-validate";
 import { toTypedSchema } from "@vee-validate/zod";
 import * as z from "zod";
 
-import { useRouter } from "vue-router";
+import { useRouter, useRoute } from "vue-router";
+import { useToast } from "@/components/ui/toast/use-toast";
 import { login } from "@/api/auth";
 import { useUserStore } from "@/stores";
+import Logo from "@/assets/logo.vue";
 
-const isLoading = ref(false);
 const userStore = useUserStore();
 const router = useRouter();
+const route = useRoute();
+const { toast } = useToast();
+
+const isLoading = ref(false);
+const showPassword = ref(false);
 
 const validationSchema = toTypedSchema(
   z.object({
@@ -96,9 +152,15 @@ const onSubmit = handleSubmit(async (values) => {
   try {
     const { data } = await login(values);
     userStore.setAccessToken(data.access_token);
-    router.push("/devices");
-  } catch (error) {
-    console.error(error);
+    router.replace({
+      path: (route.query.to ?? "/devices").toString(),
+    });
+  } catch (error: any) {
+    toast({
+      title: "Failed to login",
+      description: error.response.data.message,
+      variant: "destructive",
+    });
   } finally {
     isLoading.value = false;
   }
